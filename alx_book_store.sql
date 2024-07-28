4e187e412b4c2b129cde2b443ff22eb5e505eb67
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-07-28 14:52:14 +0300

    well

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -9,7 +9,7 @@ CREATE TABLE Books (
     books_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
-    Price DOUBLE NOT NULL,
+    price DOUBLE NOT NULL,
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
@@ -39,7 +39,9 @@ CREATE TABLE Orders (
 # Create the Order_Details table
 CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    book_id INT FOREIGN KEY (book_id) REFERENCES Books(book_id),
-    quantity DOUBLE NOT NULL
-)
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+    );
