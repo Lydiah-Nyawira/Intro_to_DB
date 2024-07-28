265ee1ab564194895496ae6a790d6116d5038a36
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-07-28 14:33:57 +0300

    First database

---
+++ alx_book_store.sql
@@ -0,0 +1,43 @@
+#  Create the database
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+# Use the created database
+USE alx_book_store;
+
+# Create the books tale
+CREATE TABLE Books (
+    books_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT FOREIGN KEY (author_id) REFERENCES Authors(author_id),
+    Price DOUBLE NOT NULL,
+    publication_date DATE
+);
+
+# Create the authors table
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+# Create the Customers table
+CREATE TABLE Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT
+);
+
+# Create the Orders table
+CREATE TABLE Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    order_date DATE NOT NULL,
+);
+
+# Create the Order_Details table
+CREATE TABLE Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    book_id INT FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    quantity DOUBLE NOT NULL
+)
