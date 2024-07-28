2b4921c6a20a026fc7e02376e33316edfa8de333
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-07-28 16:51:13 +0300

    Add script

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+# prints the full description of the table books
+SELECT
+COLUMN_NAME AS 'Column Name',
+COLUMN_TYPE AS 'Column Type',
+IS_NULLABLE AS 'Is Nullable',
+COLUMN_DEFAULT AS 'Default Value',
+EXTRA AS 'Extra Info'
+FROM
+INFORMATION_SCHEMA.COLUMNS
+WHERE
+TABLE_SCHEMA = DATABASE() AND
+TABLE_NAME = 'Books'
+ORDER BY
+ORDINAL_POSITION;
